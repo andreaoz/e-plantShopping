8e7ac9d22133abf1c49aba1d5deb4e5f7dff5989
Andrea Oz <andreaojedazam@gmail.com>
2025-08-04 20:05:17 -0700

    style: landing & navbar

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -233,7 +233,7 @@ function ProductList(props) {
     ];
     
     const styleObj = {
-        backgroundColor: '#4CAF50',
+        backgroundColor: '#59265fa9',
         color: '#fff!important',
         padding: '15px',
         display: 'flex',
@@ -251,6 +251,7 @@ function ProductList(props) {
         color: 'white',
         fontSize: '30px',
         textDecoration: 'none',
+        fontFamily: 'Book Antiqua'
     }
 
    const handleCartClick = (e) => {
@@ -272,12 +273,12 @@ const handlePlantsClick = (e) => {
         <div>
              <div className="navbar" style={styleObj}>
             <div className="tag">
-               <div style={{cursor:"pointer"}} onClick={props.toLanding} className="luxury">
-               <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-               <a   style={{textDecoration:'none'}}>
-                        <div>
-                    <h3 style={{color:'white'}}>Paradise Nursery</h3>
-                    <i style={{color:'white'}}>Where Green Meets Serenity</i>
+                <div style={{cursor:"pointer"}} onClick={props.toLanding} className="luxury">
+                    <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
+                    <a   style={{textDecoration:'none'}}>
+                    <div style={{marginLeft:"10px"}}>
+                        <h3 style={{color:'white'}}>Paradise Nursery</h3>
+                        <i style={{color:'white'}}>Where Green Meets Serenity</i>
                     </div>
                     </a>
                 </div>
@@ -285,14 +286,19 @@ const handlePlantsClick = (e) => {
             </div>
             <div style={styleObjUl}>
                 
-                <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
+                <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Find Your Perfect Plant!</a></div>
                 <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><label style={{zIndex:1,position:"fixed",fontSize:"1.5rem",cursor:"pointer"}}>{totalItems()}</label><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">0<rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
             </div>
         </div>
         {!showCart? (
         <div className="product-grid">
             <br></br>
-            {plantsArray.map((item)=><div className='mainCategoryDiv'> <h1>{item.category}</h1> 
+            {plantsArray.map((item)=>
+            <div className='mainCategoryDiv'> 
+            <div className='centered-container'>
+               <h1>{item.category}</h1>   
+            </div>
+           
             <div className="product-list">
             {item.plants.map((plant)=>
                 <div className='product-card'>
