a10126f5382164f5de10e7dc4fd8fc8826d542b4
Andrea Oz <andreaojedazam@gmail.com>
2025-08-05 12:23:37 -0700

    style: product list

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -286,8 +286,24 @@ const handlePlantsClick = (e) => {
             </div>
             <div style={styleObjUl}>
                 
-                <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Find Your Perfect Plant!</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><label style={{zIndex:1,position:"fixed",fontSize:"1.5rem",cursor:"pointer"}}>{totalItems()}</label><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">0<rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> 
+                    <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>
+                        Find Your Perfect Plant!
+                    </a>
+                </div>
+
+                <div> 
+                    <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                        <div className='shopping-section'>    
+                            <div className="cart_quantity_count">
+                                {totalItems()}
+                            </div>
+                            <div className="cart">
+                                <span>🛒</span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
         {!showCart? (
@@ -295,18 +311,19 @@ const handlePlantsClick = (e) => {
             <br></br>
             {plantsArray.map((item)=>
             <div className='mainCategoryDiv'> 
+            {/*jfsjnf
             <div className='centered-container'>
                <h1>{item.category}</h1>   
             </div>
-           
+           */}
             <div className="product-list">
             {item.plants.map((plant)=>
                 <div className='product-card'>
                     <img className='product-image' src={plant.image} alt={plant.name} />
                     <h2>{plant.name}</h2>
-                    <p>{plant.description}</p>
-                    <p>{plant.cost}</p>
-                    <button style={{backgroundColor:alreadyInCart(plant.name)?"gray":"#4caf50"}} disabled={alreadyInCart(plant.name)? true:false} onClick={()=>handleAddToCart({name:plant.name,cost:plant.cost,image:plant.image})} className='product-button'>Add to Cart</button>
+                    <p className='description-plant'>{plant.description}</p>
+                    <p className='product-price'>{plant.cost}</p>
+                    <button style={{backgroundColor:alreadyInCart(plant.name)?"gray":"#ac53cfe3"}} disabled={alreadyInCart(plant.name)? true:false} onClick={()=>handleAddToCart({name:plant.name,cost:plant.cost,image:plant.image})} className='product-button'>Add to Cart</button>
                 </div>)}
                  </div>
             </div>)}
